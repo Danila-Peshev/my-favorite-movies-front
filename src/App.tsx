3226b99f5720ed2f5e12b0c93e2f1f2c6f7598e2
Home <danila.peshev@kodep.ru>
2024-07-17 10:08:47 +0300

    😁

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <>
     <div className="w-1/4 mt-20 mx-auto">
     <img src={logo} className="mx-auto size-40 mb-10 rounded-full"></img>
-    <div className="h-80 bg-white border border-teal-200 shadow-xl">
+    <div className="h-80 bg-white border border-zinc-500 shadow-xl rounded">
       <div className="my-10 w-11/12 mx-auto">
         <div className="flex flex-col mx-auto">
           <label className="left">Имя пользователя или e-mail</label>
@@ -17,15 +17,16 @@ function App() {
           <label>Пароль</label>
           <input type="password" className="border border-neutral-300 shadow-sm h-10 text-2xl"></input>
         </div>
-        <div className="flex relative my-10">
-            <label><input 
+        <div className="flex relative my-10 items-center">
+            <input 
               type="checkbox" 
               checked={checked} 
               onChange={() => setChecked(!checked)}
               className="size-5 rounded-none border-none border-neutral-300 mr-4">
-            </input>Запомнить меня</label>
+            </input>
+            <label>Запомнить меня</label>
             <div className="grow"></div>
-            <button className="bg-blue-500 h-10 w-20 rounded text-white">Войти</button>
+            <button className="bg-blue-500 h-10 w-20 rounded text-white hover:bg-white hover:text-black hover:border hover:border-black">Войти</button>
         </div>
       </div>
     </div>
