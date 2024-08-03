1347912d77eeb67ac4d8b3566047712a80a725fb
Home <danila.peshev@kodep.ru>
2024-08-03 13:36:40 +0300

    Commit [3]: Update UserRepository(like back); Add UserService;

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -1,3 +1,5 @@
+import { User } from "../types/User";
+
 export function findByEmail(email: string) {
   const user = JSON.parse(localStorage.getItem("users") || "{}").find(
     (obj: { email: string }) => {
@@ -5,4 +7,45 @@ export function findByEmail(email: string) {
     }
   );
   return user;
+}
+
+export function saveUser(user: User): void {
+  localStorage.setItem("user", JSON.stringify(user));
+  let users: User[] = JSON.parse(localStorage.getItem("users") || "[]");
+
+  const updatedUsers = users.map(existingUser =>
+    existingUser.id === user.id ? user : existingUser
+  );
+
+  localStorage.setItem("users", JSON.stringify(updatedUsers));
+}
+
+export function updateFavoriteMovies(user: User, movieId: number): void {
+  user.favoriteMoviesId.push(movieId);
+  saveUser(user);
+}
+
+export function removeFavoriteMovie(user: User, movieId: number): void {
+  user.favoriteMoviesId = user.favoriteMoviesId.filter((id) => id !== movieId);
+  saveUser(user);
+}
+
+export function updateFavoriteGenres(user: User, genreId: number): void {
+  user.favoriteGenresId.push(genreId);
+  saveUser(user);
+}
+
+export function removeFavoriteGenre(user: User, genreId: number): void {
+  user.favoriteGenresId = user.favoriteGenresId.filter((id) => id !== genreId);
+  saveUser(user);
+}
+
+export function updateWatchedMovies(user: User, movieId: number): void {
+  user.watchedMoviesId.push(movieId);
+  saveUser(user);
+}
+
+export function removeWatchedMovie(user: User, movieId: number): void {
+  user.watchedMoviesId = user.watchedMoviesId.filter((id) => id !== movieId);
+  saveUser(user);
 }
