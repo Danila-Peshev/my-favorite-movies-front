c9715d0761c92164349777133851714c0ea5ed46
Home <danila.peshev@kodep.ru>
2024-08-03 13:47:23 +0300

    Commit [2]: Move main indexes it as it was;

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { fillLocalStorage } from "./fillLocalStorage";
-import { AuthProvider } from "./global-components/AuthContext";
-import router from "./routes";
-import { RouterProvider } from "react-router-dom";
 import { I18nextProvider } from "react-i18next";
-import i18n from "./i18n";
-import LanguageSwitcher from "./global-components/switch-language/LanguageSwitcher";
+import { RouterProvider } from "react-router-dom";
+import { AuthProvider } from "./global-components/AuthContext";
 import { LanguageProvider } from "./global-components/switch-language/LanguageContext";
+import LanguageSwitcher from "./global-components/switch-language/LanguageSwitcher";
+import i18n from "./i18n";
+import router from "./routes";
 
 fillLocalStorage();
 
