12213a16bfc627d9147c61f64b041369a020c906
Home <danila.peshev@kodep.ru>
2024-08-12 09:07:38 +0300

    correction: Check for Movies Response has been removed, the default result is empty.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,11 @@
 import GenresBlock from "./GenresBlock";
 import { useEffect, useState } from "react";
 import { Genre } from "../../types/movie-api-types/Genre";
-import { getAllGenres, getFavoriteMoviesByIds, getMoviesByFilters } from "../../services/MovieService";
+import {
+  getAllGenres,
+  getFavoriteMoviesByIds,
+  getMoviesByFilters,
+} from "../../services/MovieService";
 import { useLanguage } from "../../components/switch-language/LanguageContext";
 import MoviesBlock from "./movies-block/MoviesBlock";
 import { ViewProvider } from "./movies-block/switch-view/ViewContext";
@@ -15,7 +19,12 @@ import { useUser } from "../../components/UserContext";
 const Home = () => {
   const { language } = useLanguage();
   const [genres, setGenres] = useState<Genre[]>([]);
-  const [moviesResponse, setMoviesResponse] = useState<MoviesResponse>();
+  const [moviesResponse, setMoviesResponse] = useState<MoviesResponse>({
+    page: 1,
+    results: [],
+    totalPages: 1,
+    totalResult: 0,
+  });
   const { user } = useAuth();
   const [page, setPage] = useState<number>(1);
   const [status, setStatus] = useState({
@@ -33,9 +42,17 @@ const Home = () => {
   );
 
   async function fetchMoviesResponse() {
-    setStatus((prevStatus) => ({ ...prevStatus, loadingMovies: true, errorMovies: false }));
+    setStatus((prevStatus) => ({
+      ...prevStatus,
+      loadingMovies: true,
+      errorMovies: false,
+    }));
     try {
-      const movies = await getFavoriteMoviesByIds({ids: getFavoriteMoviesId(), language, page });
+      const movies = await getFavoriteMoviesByIds({
+        ids: getFavoriteMoviesId(),
+        language,
+        page,
+      });
       setMoviesResponse(movies);
     } catch (err) {
       setStatus((prevStatus) => ({ ...prevStatus, errorMovies: true }));
@@ -45,7 +62,11 @@ const Home = () => {
   }
 
   async function fetchGenres() {
-    setStatus((prevStatus) => ({ ...prevStatus, loadingGenres: true, errorGenres: false }));
+    setStatus((prevStatus) => ({
+      ...prevStatus,
+      loadingGenres: true,
+      errorGenres: false,
+    }));
     try {
       const fetchedGenres = await getAllGenres(language);
       setGenres(fetchedGenres);
@@ -70,19 +91,24 @@ const Home = () => {
   return (
     <ViewProvider>
       <div className="w-11/12 flex flex-col gap-y-5 p-5 mx-auto mt-24 mb-12 text-white text-base font-medium bg-gray-900 rounded">
-        {(loadingGenres || loadingMovies) ? (
+        {loadingGenres || loadingMovies ? (
           t("loading")
-        ) : (errorGenres || errorMovies) ? (
+        ) : errorGenres || errorMovies ? (
           t("requestError")
         ) : (
           <>
             <GenresBlock genres={genres} />
-            {moviesResponse ? (
-              <>
-              <MoviesBlock genres={genres} page={moviesResponse.page} movies={moviesResponse.results} showAdditionalButtons={false} />
-              <PaginationBlock page={moviesResponse.page} totalPages={totalPages} setPage={setPage}/>
-              </>
-            ) : <span>{t("loading")}</span>}
+            <MoviesBlock
+              genres={genres}
+              page={moviesResponse.page}
+              movies={moviesResponse.results}
+              showAdditionalButtons={false}
+            />
+            <PaginationBlock
+              page={moviesResponse.page}
+              totalPages={totalPages}
+              setPage={setPage}
+            />
           </>
         )}
       </div>
